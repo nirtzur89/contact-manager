693a96ff5ffa179a9489514c3073cc646136ce1c
nirtzur <nirtzur89@gmail.com>
2019-05-11 14:22:59 +0200

    first components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
-import React from "react";
+import React, { Component } from "react";
+import Contact from "./components/Contact";
+import Header from "./components/Header";
 import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>the app component</h1>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Header branding="Contact Manager" />
+        <Contact
+          name="Nir Tzur"
+          email="nirtzur89@gmail.com"
+          phone="01736349808"
+        />
+        <Contact
+          name="Krystian Kielar"
+          email="kk1234@gmail.com"
+          phone="01785466525"
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
