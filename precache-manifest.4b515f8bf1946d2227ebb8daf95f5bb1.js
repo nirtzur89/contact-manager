50f504bd4560988b0da9357e2b02111fb3fe66d5
nirtzur <nirtzur89@gmail.com>
2019-05-11 21:25:44 +0200

    Updates

---
+++ precache-manifest.4b515f8bf1946d2227ebb8daf95f5bb1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb6fd68f851cf66c3c73251b08d95b7d",
+    "url": "/contact-manager/index.html"
+  },
+  {
+    "revision": "907aa26d76bb64d30b8d",
+    "url": "/contact-manager/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "72a03783cfcf5c1e0c4d",
+    "url": "/contact-manager/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "907aa26d76bb64d30b8d",
+    "url": "/contact-manager/static/js/2.c8eb7ff7.chunk.js"
+  },
+  {
+    "revision": "72a03783cfcf5c1e0c4d",
+    "url": "/contact-manager/static/js/main.3c56de5c.chunk.js"
+  },
+  {
+    "revision": "a25dc548b2853493d42e",
+    "url": "/contact-manager/static/js/runtime~main.b61356a3.js"
+  }
+]);
